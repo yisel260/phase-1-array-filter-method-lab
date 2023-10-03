25d83dacddd1586ba0f8c63e1b3370aad3f24b16
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2023-10-02 21:33:52 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Code your solution here
+// Code your solution her
+
+
+
+//function findMatching(drivers, passedInName) {
+   // return drivers.filter(function(driver) {
+    //  return driver.name.toLowerCase() === passedInName.toLowerCase();
+   // });
+ // }
+
+
+  //function findMatching(drivers, passedInName) {
+   // return drivers.filter(function(driver) {
+   //   return driver.name.toLowerCase() === passedInName.toLowerCase();
+   // });
+ // }
+
+  //function findMatching(drivers, passedInName) {
+   // const filteredDrivers = drivers.filter(function(driver) {
+   //   return driver.name.toLowerCase() === passedInName.toLowerCase();
+   // });
+  
+  //  return filteredDrivers;
+  //}
+
+  function findMatching(drivers, passedInName) {
+    const filteredDrivers = drivers.filter(function(driver) {
+      return driver.toLowerCase() === passedInName.toLowerCase();
+    });
+  
+    return filteredDrivers;
+  }
+
+  function fuzzyMatch(drivers, string ){
+    const fuzzyDrivers = drivers.filter(function (driver){
+        return driver.charAt(0) === string.charAt(0)
+    });
+    return fuzzyDrivers
+  }
+
+  function matchName(drivers, string){
+    const matchedNames = drivers.filter(function (drivers){
+       return  drivers.name === string 
+    })
+    return matchedNames
+  }
